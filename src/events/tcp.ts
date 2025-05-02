675a849a50d901cbe17e9e5b144f27348ab5991f
Luis Bazan <me@luisb.space>
2025-05-01 20:05:24 -0500

    Added tcp reply message

--- src/events/tcp.ts
+++ src/events/tcp.ts
@@ -1,8 +1,15 @@
 import net from "net";
 import {
+  TCPMessage,
   TCPMessageHelloPayload,
   TCPMessageMessagePayload,
+  TCPMessageType,
 } from "../types/tcp.js";
+import { addChat } from "../contexts/ChatContext.js";
+import { getPairs } from "../config.js";
+
+const sessionChats: string[] = [];
+const MAX_SESSION_CHAT = 1000;
 
 export async function onTcpHello(
   socket: net.Socket,
@@ -19,11 +26,36 @@ export async function onTcpMessage(
   socket: net.Socket,
   payload: TCPMessageMessagePayload
 ) {
-  console.log(
-    `💬 ${payload.username}: ${payload.message} (${
-      payload.uuid
-    } ${socket.remoteAddress!})`
-  );
+  // Filter if the message already exists in the session
+  if (sessionChats.includes(payload.uuid)) {
+    return;
+  }
 
-  // TODO: Reply message to pairs
+  // Add the message to the session
+  addChat(payload);
+  sessionChats.push(payload.uuid);
+
+  if (sessionChats.length > MAX_SESSION_CHAT) {
+    sessionChats.shift();
+  }
+
+  // Discard the message if the TTL is less than 1
+  if (payload.ttl <= 1) return;
+
+  // Reply message to pairs
+  const messagePackage: TCPMessage = {
+    type: TCPMessageType.MESSAGE,
+    payload: {
+      uuid: payload.uuid,
+      username: payload.username,
+      message: payload.message,
+      ttl: payload.ttl - 1,
+    },
+  };
+  getPairs().forEach((pair) => {
+    // Filter if the message is from the same user
+    if (pair.remoteAddress === socket.remoteAddress) return;
+
+    pair.write(JSON.stringify(messagePackage));
+  });
 }
