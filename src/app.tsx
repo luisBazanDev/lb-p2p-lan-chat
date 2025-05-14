4a38cfa8b7fccdb8852feccc333ce84fa48073ff
Luis Bazan <me@luisb.space>
2025-05-14 17:19:28 -0500

    Test #1 to fix flinking

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,13 @@ import ChatsBox from "./components/organisms/ChatsBox.js";
 export default function App() {
   return (
     <>
-      <Box margin={1} display="flex" flexDirection="column" width="100%">
+      <Box
+        margin={1}
+        display="flex"
+        flexDirection="column"
+        width="100%"
+        height={-1}
+      >
         <ChatsBox />
         <InputText />
       </Box>
